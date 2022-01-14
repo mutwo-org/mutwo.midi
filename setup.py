4fe42c6a6970a91116eff36b80dc58b7e7dd1381
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-01-14 20:56:20 +0000

    Increment patch version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extras_require = {"testing": ["nose", "coveralls"]}
 
 setuptools.setup(
     name="mutwo.ext-midi",
-    version="0.2.0",
+    version="0.2.1",
     license="GPL",
     description="example extension for event based framework for generative art",
     long_description=long_description,
