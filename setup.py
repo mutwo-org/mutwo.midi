f4724651f72f29d2314560bd1410654a3e12ace6
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-05-02 10:35:53 +0000

    Update to new mutwo music version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools  # type: ignore
 
-MAJOR, MINOR, PATCH = 0, 7, 2
+MAJOR, MINOR, PATCH = 0, 7, 3
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 """This project uses semantic versioning.
 See https://semver.org/
