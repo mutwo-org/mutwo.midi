bedaeca8e4cea722ea4279b22e5b4096322365fe
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-15 08:47:18 +0100

    Update mutwo.core in nix to v.1.1.0

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/61ebb657ef5806eb067f5df6885254fdbae8f44c.tar.gz";
+  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/d90b6db7d433d64c25f039adcd6b41075c05c013.tar.gz";
+  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/6a5f2ca81e6e4e06b7ad4d6f46b0361ceaca4ea1.tar.gz";
   mutwo-music = import (mutwo-music-archive + "/default.nix");
 
 in
