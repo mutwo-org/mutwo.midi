c039c5ce5b5f0cc98ceab3ccf907a2a4d8f6b770
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-12-07 23:40:47 +0100

    Update nix

--- default.nix
+++ default.nix
@@ -3,10 +3,7 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
-  mutwo-core = import (mutwo-core-archive + "/default.nix");
-
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/10c4f7f9fd4ec5d051b73dbffe2edd0628f9752f.tar.gz";
+  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/929ec3faadd6426356d27df9614143e641be745d.tar.gz";
   mutwo-music = import (mutwo-music-archive + "/default.nix");
 
 in
@@ -23,7 +20,6 @@ in
       python310Packages.pytest
     ];
     propagatedBuildInputs = [
-      mutwo-core
       mutwo-music
       python310Packages.mido
       python310Packages.numpy
