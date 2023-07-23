b9f2b6232cdef188de61b800969acf1dd4b7145b
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-07-23 09:37:06 +0200

    nix:Use derivation from mutwo-nix but \w local src

--- default.nix
+++ default.nix
@@ -1,33 +1,11 @@
-with import <nixpkgs> {};
-with pkgs.python310Packages;
-
 let
-
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/475439a90bb7bf2d429bad93637a22e08f1a4e89.tar.gz";
-  mutwo-music = import (mutwo-music-archive + "/default.nix");
-
+  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
+  mutwo-midi = import (sourcesTarball + "/mutwo.midi/default.nix") {};
+  mutwo-midi-local = mutwo-midi.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
+  mutwo-midi-local
 
-  buildPythonPackage rec {
-    name = "mutwo.midi";
-    src = fetchFromGitHub {
-      owner = "mutwo-org";
-      repo = name;
-      rev = "5cf9609b84fb51811bc3d746b32e40e325427a4d";
-      sha256 = "sha256-dgnV3DbDFMCCrst9lfYDXoRz0RW+2jGNhZ6e4YoVkc4=";
-    };
-    checkInputs = [
-      python310Packages.pytest
-    ];
-    propagatedBuildInputs = [
-      mutwo-music
-      python310Packages.mido
-      python310Packages.numpy
-    ];
-    checkPhase = ''
-      runHook preCheck
-      pytest
-      runHook postCheck
-    '';
-    doCheck = true;
-  }
